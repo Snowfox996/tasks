f716cad4fb1b32d5aa54adae6a6a3c16f807d167
Michael <SnowFox996@gmail.com>
2022-10-11 14:10:37 -0400

    added an image

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-
+import logo from "./assets/vslogo.png";
 function App(): JSX.Element {
     return (
         <div className="App">
@@ -8,6 +8,7 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <h1>Hello this is my header</h1>
+            <img src={logo} alt="A Picture of the VSCode logo" />
             <p>Hello World! COS420</p>
             <p>Michael Lally</p>
         </div>
