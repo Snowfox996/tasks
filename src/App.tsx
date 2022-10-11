b52e599cab14e208fb2d150dd97014f6c09066e4
Michael <SnowFox996@gmail.com>
2022-10-11 13:30:52 -0400

    Added a header

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,9 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UM COS420 with React Hooks and TypeScript
+                UD CISC275 with React Hooks and TypeScript
             </header>
+            <h1>Hello this is my header</h1>
             <p>Hello World! COS420</p>
             <p>Michael Lally</p>
         </div>
