1c1583a709779f141e7633dbe71acdcc74b00fbc
Michael <SnowFox996@gmail.com>
2022-10-11 14:33:24 -0400

    added Columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import React from "react";
 import "./App.css";
 import logo from "./assets/vslogo.png";
-import { Button } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                UD COS420 with React Hooks and TypeScript
             </header>
             <h1>Hello this is my header</h1>
             <img src={logo} alt="A Picture of the VSCode logo" />
-            <ul>
-                <li>First item in my list</li>
-                <li>Second item in my List</li>
-                <li>Third item in my List</li>
-            </ul>
-            <Button onClick={() => console.log("I am logged")}>
-                Log Hello World
-            </Button>
-            <p>Hello World! COS420</p>
+            <Container>
+                <Row>
+                    <Col>
+                        <ul>
+                            <li>First item in my list</li>
+                            <li>Second item in my List</li>
+                            <li>Third item in my List</li>
+                        </ul>
+                    </Col>
+                    <Col>
+                        <Button onClick={() => console.log("I am logged")}>
+                            Log Hello World
+                        </Button>
+                    </Col>
+                </Row>
+            </Container>
+            <p>Hello World!</p>
             <p>Michael Lally</p>
         </div>
     );
