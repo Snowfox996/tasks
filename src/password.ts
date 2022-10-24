82cc9b1fb7570ac2065c877cf4dee7a36de6eed3
Michael <47731736+Snowfox996@users.noreply.github.com>
2022-10-24 11:27:23 -0400

    fixed testpassvalidity bg

--- src/password.ts
+++ src/password.ts
@@ -1,10 +1,14 @@
 export function testPassValidity(password: string): boolean {
     const specialChar = "1234567890!@#$%^&*()";
-    let isPassing = true;
+    let isPassing: boolean;
     if (password.length < 16) {
         isPassing = false;
-    } else if (password[0].match(specialChar)) {
+    } else if (specialChar.includes(password[0])) {
         isPassing = false;
+    } else if (password.includes(specialChar) == false) {
+        return false;
+    } else {
+        isPassing = true;
     }
     return isPassing;
 }
