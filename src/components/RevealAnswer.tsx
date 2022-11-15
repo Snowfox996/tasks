b8f61160335df008123da66ff609cf62a4d1686a
Michael <47731736+Snowfox996@users.noreply.github.com>
2022-11-15 13:01:46 -0500

    Fixed Double half component

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,17 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [visible, set_visible] = useState<boolean>(true);
+    const [answer, set_answer] = useState<string>("Reveal Answer");
+
+    function flip_visible(): void {
+        set_visible(!visible);
+        set_answer("42");
+    }
+    return (
+        <div>
+            <Button onClick={flip_visible}>{answer}</Button>
+            {visible && <div>{answer}</div>}
+        </div>
+    );
 }
