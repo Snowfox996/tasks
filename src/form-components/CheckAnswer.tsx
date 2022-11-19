0434b6a9236dbb65e4a8e2caa1cfd0c532f2f839
Michael <47731736+Snowfox996@users.noreply.github.com>
2022-11-19 15:23:06 -0500

    Check Answer Implemented

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,43 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+interface AnswerBoxState {
+    answer: string;
+    setAnswer: (newAnswer: string) => void;
+}
+
+export function AnswerBox({ answer, setAnswer }: AnswerBoxState): JSX.Element {
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setAnswer(event.target.value);
+    }
+    return (
+        <div>
+            <Form.Group>
+                <Form.Label>Input Answer</Form.Label>
+                <Form.Control value={answer} onChange={updateAnswer} />
+            </Form.Group>
+        </div>
+    );
+}
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
-    return (
-        <div>
-            <h3>Check Answer</h3>
-        </div>
-    );
+    const [answer, setAnswer] = useState<string>("");
+    const answerBox = <AnswerBox answer={answer} setAnswer={setAnswer} />;
+    if (expectedAnswer == answer) {
+        return (
+            <div>
+                <div>{answerBox}</div>
+                ✔️
+            </div>
+        );
+    } else
+        return (
+            <div>
+                <div>{answerBox}</div>❌
+            </div>
+        );
 }
