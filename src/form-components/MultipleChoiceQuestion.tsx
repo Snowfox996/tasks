d8706471cb073b367f70ed55ed941de3775bea89
Michael <47731736+Snowfox996@users.noreply.github.com>
2022-11-21 13:15:28 -0500

    MultipleChoiceQuestion Implemented

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,28 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const CURRENT_CHOICE: string = options[0];
+    const correct = "✔️";
+    const incorrect = "❌";
+    const [choice, setChoice] = useState<string>(CURRENT_CHOICE);
+
+    function updateChoice(event: React.ChangeEvent<HTMLSelectElement>) {
+        setChoice(event.target.value);
+    }
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group>
+                <Form.Select value={choice} onChange={updateChoice}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            The answer is {choice === expectedAnswer ? correct : incorrect}
         </div>
     );
 }
